5553a29467bc6a08c985a0ca1a1a7292c09f4df9
Brandon Sultana <brandonsultana1@gmail.com>
2017-10-25 14:19:00 -0400

    Add files via upload

---
+++ Node.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "Node.h"
+
+using namespace std;
+
+Node :: Node() {
+	nodeID = -1;
+	hostname = "";
+	controlPort = -1;
+	dataPort = -1;
+}
+
+Node :: Node (int nodeID_, string hostname_, int controlPort_, int dataPort_, vectory<int> neighbors_) {
+	nodeID = nodeID_;
+	hostname = hostname_;
+	controlPort = controlPort_;
+	dataPort = dataPort_;
+	neighbors = neighbors_;
+}
+
+void Node :: addNeighbor(int neighborID) {
+	neighbors.push_back(neighborID);
+}
+
+void Node :: removeNeighbor(int neighborID) {
+	int i;
+	int index = -1;
+	for(i=0; i < neighbors.size(); i++) {
+		if(neighbors.at(i) == neighborID) {
+			index = i;
+			break;
+		}
+	}
+	
+	if(index != -1) {
+		neighbors.erase(neighbors.begin()+index);
+	}
+}
---
+++ Node.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Node {
+	public:
+		Node();
+		Node(int nodeID_, string hostname_, int controlPort_, int dataPort_, vectory<int> neighbors_);
+		int nodeID;
+		string hostname;
+		int controlPort;
+		int dataPort;
+		vector<int> neighbors;
+		
+		void addNeighbor(int neighborID);
+		void removeNeighbor(int neighborID);
+}
